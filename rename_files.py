a7ad6fbfb6a62d995b8ccc1786a3c171c6659c95
GodKelvin <kelvin.lehrback@gmail.com>
2022-05-22 17:52:18 -0300

    wip: Criacao. Renomeacao e move arquivos

--- rename_files.py
+++ rename_files.py
@@ -1,1 +1,35 @@
-print("Hello Galaxy!")
+from pathlib import Path
+import os
+from xxlimited import new
+
+def create_dir(path_dir):
+    path = Path(path_dir)
+    path.mkdir(exist_ok=True)
+
+def clean_files(path_dir, output_dir, trash):
+    create_dir(output_dir)
+    files = os.listdir(path_dir)
+    
+    for file in files:
+        without_trash = file.lower().split(trash)
+        print(without_trash)
+        new_name_and_move = output_dir + "".join(without_trash)
+
+        if(os.path.isfile(new_name_and_move)):
+            print("Arquivo ja existe")
+        else:
+            os.rename(path_dir+file, new_name_and_move)
+
+
+def main():
+    input_dir = "test/"
+    output_dir = "output/"
+    remove_from_file = "arquivo"
+    clean_files(input_dir, output_dir, remove_from_file)
+
+main()
+
+
+
+
+    
